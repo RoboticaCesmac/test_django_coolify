168e097bbaf47720a647daaaa9bd782453610b00
Núcleo de Robótica do Cesmac <roboticacesmac@gmail.com>
2024-10-09 10:44:59 -0300

    Tentativa de servir arquivos de midia 01

--- toothease/urls.py
+++ toothease/urls.py
@@ -26,7 +26,7 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("api-auth/", include("rest_framework.urls")),
     path("api/", include("core.urls")),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
